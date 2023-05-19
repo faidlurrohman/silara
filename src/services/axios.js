f70f9dde1c0bf80b8e21af47ba00ed28ba72f9ae
faidlur.rochman <faidlur.rochman@citratubindo.co.id>
2023-05-19 20:17:37 +0700

    add bearer

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,16 +1,26 @@
 import { message } from "antd";
 import axios from "axios";
+import Cookies from "js-cookie";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL_API,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
+    Authorization: `Bearer ${Cookies.get(process.env.REACT_APP_ACCESS_TOKEN)}`,
   },
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  return config;
+  return {
+    ...config,
+    headers: {
+      ...config.headers,
+      Authorization: `Bearer ${Cookies.get(
+        process.env.REACT_APP_ACCESS_TOKEN
+      )}`,
+    },
+  };
 });
 
 axiosInstance.interceptors.response.use(
