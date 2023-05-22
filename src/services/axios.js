b86fd3a654e2f804666a86f33e30893044eed138
faidlur.rochman <faidlur.rochman@citratubindo.co.id>
2023-05-22 09:31:39 +0700

    add session logout

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,8 @@
 import { message } from "antd";
 import axios from "axios";
 import Cookies from "js-cookie";
+import { store } from "../store";
+import { logoutAction } from "../store/actions/session";
 
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL_API,
@@ -28,7 +30,15 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   async (error) => {
-    message.error(error?.response?.data?.message || error.message);
+    if (
+      error?.response?.status === 401 ||
+      Cookies.get(process.env.REACT_APP_ACCESS_TOKEN) === undefined
+    ) {
+      store.dispatch(logoutAction());
+      message.error("Sesi anda berakhir, silahkan login ulang kembali");
+    } else {
+      message.error(error?.response?.data?.message || error.message);
+    }
 
     return error;
   }
