e56af9769def2ecd8cbd1755416160da32e4c117
faidlur.rochman <faidlur.rochman@citratubindo.co.id>
2023-05-23 22:34:25 +0700

    role cm

--- application/controllers/api/Role.php
+++ application/controllers/api/Role.php
@@ -11,19 +11,22 @@ class Role extends REST_Controller {
     {
         parent::__construct();
         $this->_authenticate_CORS();
+        $this->_authenticate_BEARER();
+        $this->load->model('Auth_model');
         $this->load->model('Role_model');
     }
 
-    public function data_get()
+    public function list_get()
     {
-        $this->do_get_all();
+        $this->do_get_list();
     }
 
-    private function do_get_all()
+    private function do_get_list()
     {   
-        $data = $this->Role_model->get_all();
+        $user = $this->Auth_model->check_token();
+        $data = $this->Role_model->get_list($user);
      
-        if ($data['r_code'] != 0) {
+        if ($data['code'] != 0) {
             $this->response($data, REST_Controller::HTTP_INTERNAL_SERVER_ERROR);
         } else {
             $this->response($data, REST_Controller::HTTP_OK);
@@ -38,6 +41,17 @@ class Role extends REST_Controller {
         header('Access-Control-Allow-Headers: ACCEPT, ORIGIN, X-REQUESTED-WITH, CONTENT-TYPE, AUTHORIZATION, Client-ID, Secret-Key, Authorization, User-ID');
         if ("OPTIONS" === $_SERVER['REQUEST_METHOD']) {
             die();
+        }
+    }
+
+    protected function _authenticate_BEARER()
+    {   
+        $this->load->helper('common');
+
+        $token = get_token();
+
+        if (!isset($token) || $token == 'undefined') {
+            $this->response(['status' => '401'], REST_Controller::HTTP_UNAUTHORIZED);
         }
     }
 
--- application/models/Role_model.php
+++ application/models/Role_model.php
@@ -2,22 +2,33 @@
 
 class Role_model extends CI_Model {
 
+    private $schema = 'get_role';
+    private $table  = 'silarakab.role';
+    private $read   = 'silarakab.main_read';
+
     function __construct()
     {
         parent::__construct();
         $this->load->helper('common');
     }
 
-    function get_all()
+    function get_list($user)
     {
+        $_json = '{"active":"true"}';
         $sql = "
-            with a as (
-                select * from silarakab.role
-            ) select a.*,a.id as value,a.remark as label,count(*) over() as ttl_count from a
-            order by a.remark asc
+            WITH r AS (
+                SELECT * FROM {$this->read}(0, 0, '".$user['username']."', '".$this->schema."', 'remark', '[".$_json."]'::jsonb)
+            ) SELECT 
+                (r.__res_data->>'id')::INT AS id,
+                (r.__res_data->>'id')::INT AS value, 
+                r.__res_data->>'remark' AS label, 
+                r.__code, 
+                r.__res_msg, 
+                COALESCE(r.__res_count,0)::INT AS __res_count
+            FROM r
         ";
         $query = $this->db->query($sql);
-        return model_response($query);
+        return model_response($query, 1);
     }
 
 }
