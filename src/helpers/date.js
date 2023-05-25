bb2584c5d750c05b3aa8ceaa4e941f585d97bab5
Faidlur Rohman <faidrohman@outlook.com>
2023-05-25 22:37:08 +0700

    add date helper

---
+++ src/helpers/date.js
@@ -0,0 +1,16 @@
+import dayjs from "dayjs";
+import { DATE_FORMAT_DB, DATE_FORMAT_VIEW, DATE_UTC } from "./constants";
+
+export const convertDate = (date) => {
+  if (date) return dayjs(date);
+
+  return dayjs();
+};
+
+export const dbDate = (date) => {
+  return dayjs(date).utc(DATE_UTC).format(DATE_FORMAT_DB);
+};
+
+export const viewDate = (date) => {
+  return dayjs(date).utc(DATE_UTC).format(DATE_FORMAT_VIEW);
+};
