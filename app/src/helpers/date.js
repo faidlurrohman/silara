720f7c6851d4b664fbd173b0d9b06489b5cab51f
Faidlur Rohman <faidrohman@outlook.com>
2023-07-05 09:48:16 +0700

    add time location

--- app/src/helpers/date.js
+++ app/src/helpers/date.js
@@ -4,9 +4,9 @@ import { DATE_FORMAT_DB, DATE_FORMAT_VIEW, DATE_UTC } from "./constants";
 export const convertDate = (date, useFormat) => {
 	if (useFormat) return dayjs(date).utc(DATE_UTC).format(useFormat);
 
-	if (date) return dayjs(date);
+	if (date) return dayjs(date).utc(DATE_UTC);
 
-	return dayjs();
+	return dayjs().utc(DATE_UTC);
 };
 
 export const dbDate = (date) => {
