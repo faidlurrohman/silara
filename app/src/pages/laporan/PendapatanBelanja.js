0f54fccbbeaa10ee435395289bc018d6f275631d
Faidlur Rohman <faidrohman@outlook.com>
2023-07-22 02:57:39 +0700

    add column chart

--- app/src/pages/laporan/PendapatanBelanja.js
+++ app/src/pages/laporan/PendapatanBelanja.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { DatePicker, Select, Table } from "antd";
+import { Card, DatePicker, Select, Table } from "antd";
 import axios from "axios";
 import ReloadButton from "../../components/button/ReloadButton";
 import { getCityList } from "../../services/city";
@@ -12,13 +12,30 @@ import { formatterNumber } from "../../helpers/number";
 import _ from "lodash";
 import ExportButton from "../../components/button/ExportButton";
 import { lower } from "../../helpers/typo";
+import { getAccountList } from "../../services/account";
+import { Column } from "@ant-design/plots";
 
 const { RangePicker } = DatePicker;
+
+const columnConfig = {
+	appendPadding: 20,
+	style: { height: 300 },
+	isGroup: true,
+	xField: "city",
+	yField: "value",
+	seriesField: "name",
+	legend: false,
+	color: ["#1ca9e6", "#f88c24"],
+	xAxis: { label: { autoRotate: true } },
+	scrollbar: { type: "horizontal" },
+};
 
 export default function PendapatanBelanja() {
 	const [data, setData] = useState([]);
 	const [cities, setCities] = useState([]);
 	const [exports, setExports] = useState([]);
+	const [chartP, setChartP] = useState([]);
+	const [chartB, setChartB] = useState([]);
 	const [loading, setLoading] = useState(false);
 
 	const tableFilterInputRef = useRef(null);
@@ -41,21 +58,124 @@ export default function PendapatanBelanja() {
 					pagination: { ...params.pagination, pageSize: 0 },
 				}),
 				getCityList(),
+				getAccountList("base"),
 			])
 			.then(
-				axios.spread((_data, _export, _cities) => {
+				axios.spread((_data, _export, _cities, _bases) => {
 					setLoading(false);
 					setData(responseGet(_data).data);
 					setCities(_cities?.data?.data || []);
 					setExports(setDataExport(responseGet(_export).data));
+
 					setTablePage({
 						pagination: {
 							...params.pagination,
 							total: responseGet(_data).total_count,
 						},
 					});
+
+					if (
+						!!(_bases?.data?.data || []).length &&
+						!!(_cities?.data?.data || []).length
+					) {
+						makeChartData(
+							_bases?.data?.data || [],
+							_cities?.data?.data || [],
+							params?.filters,
+							responseGet(_export).data
+						);
+					}
 				})
 			);
+	};
+
+	const makeChartData = (bases, cities, filter, values) => {
+		// init tampung data masing-masing  akun base
+		let _cp = null,
+			_cb = null;
+
+		// init array untung menampung data chart
+		let _chartP = [],
+			_chartB = [];
+
+		// tampung semua kota
+		let _sl = cities;
+
+		// cari data akun base yang labelnya Pendapatan Dan Belanja
+		_cp = _.find(bases, (v) => v?.label.includes("Pendapatan"));
+		_cb = _.find(bases, (v) => v?.label.includes("Belanja"));
+
+		// cek filter kotanya
+		if (filter && filter?.city_id && !!filter?.city_id[0].length) {
+			// ambil kota yang hanya difilter sebagai default tampilan
+			_sl = _.filter(cities, (v) => filter?.city_id[0].includes(v?.id));
+		}
+
+		// proses untuk pendapatan, jika tidak ada tidak usah tampilkan chart
+		if (_cp) {
+			// loop kota
+			_.map(_sl, (city) => {
+				// cari kota dan sesuai akun base yang ada didata list
+				const cb = _.filter(
+					values,
+					(v) => v?.city_id === city?.id && v?.account_base_id === _cp?.id
+				);
+
+				// init default chart plan atau real
+				let _ip = { name: "Anggaran", value: 0, city: city?.label };
+				let _ir = { name: "Relisasi", value: 0, city: city?.label };
+
+				// kalau ada data dari city yang ada dilist
+				if (cb && !!cb.length) {
+					// loop hitung masing-masing plan atau real
+					_.map(cb, (cur) => {
+						// pakai float karena nilai ada pakai koma
+						_ip.value += parseFloat(cur?.account_base_plan_amount || 0);
+						_ir.value += parseFloat(cur?.account_base_real_amount || 0);
+					});
+				}
+
+				// push init masing-masing plan atau real
+				_chartP.push(_ip);
+				_chartP.push(_ir);
+			});
+
+			// set ke state
+			setChartP(_chartP);
+		}
+
+		// proses untuk belanja, jika tidak ada tidak usah tampilkan chart
+		if (_cb) {
+			// loop kota
+			_.map(_sl, (city) => {
+				// cari kota dan sesuai akun base yang ada didata list
+				const cb = _.filter(
+					values,
+					(v) => v?.city_id === city?.id && v?.account_base_id === _cb?.id
+				);
+
+				// init default chart plan atau real
+				let _ip = { name: "Anggaran", value: 0, city: city?.label };
+				let _ir = { name: "Relisasi", value: 0, city: city?.label };
+
+				// kalau ada data dari city yang ada dilist
+				if (cb && !!cb.length) {
+					// loop hitung masing-masing plan atau real
+					_.map(cb, (cur) => {
+						// pakai float karena nilai ada pakai koma
+						_ip.value += parseFloat(cur?.account_base_plan_amount || 0);
+						_ir.value += parseFloat(cur?.account_base_real_amount || 0);
+					});
+				}
+
+				// push init masing-masing plan atau real
+				_chartB.push(_ip);
+				_chartB.push(_ir);
+			});
+
+			// set ke state
+			setChartB(_chartB);
+		}
 	};
 
 	const reloadTable = () => {
@@ -304,7 +424,7 @@ export default function PendapatanBelanja() {
 
 	return (
 		<>
-			<div className="flex flex-col space-y-2 sm:space-y-0 sm:space-x-2 sm:flex-row md:space-y-0 md:space-x-2 md:flex-row">
+			<div className="flex flex-col mb-1 space-y-2 sm:space-y-0 sm:space-x-2 sm:flex-row md:space-y-0 md:space-x-2 md:flex-row">
 				<div className="flex flex-row md:space-x-2">
 					<h2 className="text-xs font-normal text-right w-14 hidden md:inline">
 						Tanggal :
@@ -359,23 +479,47 @@ export default function PendapatanBelanja() {
 					/>
 				)}
 			</div>
-			<div className="mt-4">
-				<Table
-					scroll={{
-						scrollToFirstRowOnChange: true,
-						x: "100%",
-					}}
-					bordered
-					size="small"
-					loading={loading}
-					dataSource={data}
-					columns={columns}
-					rowKey={(record) => `${record?.account_base_id}_${record?.city_id}`}
-					onChange={onTableChange}
-					pagination={tablePage.pagination}
-					tableLayout="auto"
-				/>
-			</div>
+			{!!chartP.length && !!chartB.length && (
+				<div className="flex mx-0.5 pb-2 space-x-0 space-y-2 md:space-x-2 md:space-y-0">
+					<Card
+						title={
+							<span className="text-xs">Anggaran & Realisasi Pendapatan</span>
+						}
+						bodyStyle={{ padding: 0, margin: 0 }}
+						className="text-center w-full"
+					>
+						<Column {...columnConfig} data={chartP} loading={loading} />
+					</Card>
+				</div>
+			)}
+			{!!chartB.length && (
+				<div className="flex mx-0.5 pb-2 space-x-0 space-y-2 md:space-x-2 md:space-y-0">
+					<Card
+						title={
+							<span className="text-xs">Anggaran & Realisasi Belanja</span>
+						}
+						bodyStyle={{ padding: 0, margin: 0 }}
+						className="text-center w-full"
+					>
+						<Column {...columnConfig} data={chartB} loading={loading} />
+					</Card>
+				</div>
+			)}
+			<Table
+				scroll={{
+					scrollToFirstRowOnChange: true,
+					x: "100%",
+				}}
+				bordered
+				size="small"
+				loading={loading}
+				dataSource={data}
+				columns={columns}
+				rowKey={(record) => `${record?.account_base_id}_${record?.city_id}`}
+				onChange={onTableChange}
+				pagination={tablePage.pagination}
+				tableLayout="auto"
+			/>
 		</>
 	);
 }
